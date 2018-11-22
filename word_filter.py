02c5bb90b5f777dd309cc7da3009fdb35b02ef49
naoya0082 <6ds3f7@gmail.com>
2018-11-22 16:41:34 +0900

    C-2 完了 replase = 置き換え まだ1人じゃ出来ない

--- word_filter.py
+++ word_filter.py
@@ -2,8 +2,11 @@ class WordFilter:
     def __init__(self, my_filter):
         self.my_filter = my_filter
 
-    def detect(self, word):
-        return self.my_filter in word
+    def detect(self, NG_word):
+        return self.my_filter in NG_word
+
+    def censor(self, NG_word):
+        return NG_word.replace(self.my_filter, "<censored>")
 
 
 if __name__ == "__main__":
@@ -13,3 +16,7 @@ if __name__ == "__main__":
     my_filter.detect("昨日のアーセナルの試合アツかった！")  # Trueを返す ※出力されるわけではありません！
     # NGワードが含まれていない場合
     my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す ※出力されるわけではありません！
+
+    my_filter.censor("昨日のアーセナルの試合アツかった！")  # "昨日の<censored>の試合アツかった！" を返す ※出力されるわけではありません！
+    # NGワードが含まれていない場合
+    my_filter.censor("昨日のリバプールの試合アツかった！")  # "昨日のリバプールの試合アツかった！" を返す ※出力されるわけではありません！
